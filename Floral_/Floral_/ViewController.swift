a98ff89d2a8a05c6543264afeb5ffae029d37887
Jiang <jiang@JiangdeMacBook-Pro.local>
2016-07-14 21:27:59 +0800

    new project

---
+++ Floral_/Floral_/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Floral_
+//
+//  Created by Jiang on 16/7/14.
+//  Copyright © 2016年 Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
